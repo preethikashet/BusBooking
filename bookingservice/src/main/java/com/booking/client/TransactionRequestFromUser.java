7bdcccf9f40c5bfbd39ed603ce47d9354d2d3b7f
deepthi20kulal <20deepthikulal@gmail.com>
2025-09-17 16:29:07 +0530

    added uname to dto, and correction interface

--- bookingservice/src/main/java/com/booking/client/TransactionRequestFromUser.java
+++ bookingservice/src/main/java/com/booking/client/TransactionRequestFromUser.java
@@ -1,5 +1,6 @@
 package com.booking.client;
 
+import org.example.dto.TransactionResponseDTO;
 import org.example.dto.UserBookBusRequestDTO;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "paymentservice",path = "/api/payment")
 public interface TransactionRequestFromUser {
-    @PostMapping("/processBooking")
-    void processBooking(@RequestBody UserBookBusRequestDTO request);
+    @PostMapping("/makeTransaction")
+    TransactionResponseDTO makeTransaction(@RequestBody UserBookBusRequestDTO request);
 }
 
--- common-dtos/src/main/java/org/example/dto/TransactionResponseDTO.java
+++ common-dtos/src/main/java/org/example/dto/TransactionResponseDTO.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class TransactionResponseDTO {
     public Integer userid;
+    public String username;
     public Integer vendorid;
     public Integer busid;
     public Integer price;
