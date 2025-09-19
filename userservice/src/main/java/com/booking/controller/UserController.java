90fd2d2346d564ab875793dd3a2515a28a02ea0c
Soumyaneelakari1 <soumyaneelakari@gmail.com>
2025-09-19 15:51:45 +0530

    Updated user controller

--- userservice/src/main/java/com/booking/controller/UserController.java
+++ userservice/src/main/java/com/booking/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.booking.controller;
 
+import com.booking.client.BookingClient;
 import com.booking.entity.UserEntity;
 import com.booking.service.UserService;
 import org.apache.http.HttpStatus;
 import org.example.dto.UserBookBusRequestDTO;
+import org.example.dto.UserBookBusResponseDTO;
 import org.example.dto.UserDTO;
 import org.example.dto.UserResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
@@ -16,7 +19,8 @@ import java.util.List;
 @RequestMapping
 @RestController("/api/user")
 public class UserController {
-
+    @Autowired
+    public BookingClient bookingClient;
     @Autowired
     public UserService userService;
     /**
@@ -24,11 +28,11 @@ public class UserController {
      * @param userEntity
      * @return
      */
-    @PostMapping("/register")
-    public ResponseEntity<String> addbooking(@RequestBody UserEntity userEntity){
-//        return bookingService.addbooking(bookingEntity);
-        return ResponseEntity.ok("user created");
-    }
+//    @PostMapping("/register")
+//    public ResponseEntity<String> addbooking(@RequestBody UserEntity userEntity){
+////        return bookingService.addbooking(bookingEntity);
+//        return ResponseEntity.ok("user created");
+//    }
     /**
      *
      * @param bookingDate
@@ -58,11 +62,11 @@ public class UserController {
      * @return
      */
     @PostMapping("/book")
-    public String bookBus(@RequestBody UserDTO userDTO)
+    public ResponseEntity<Object> bookBus(@RequestBody UserDTO userDTO)
     {
         List<UserEntity> users = userService.getUsers();
         UserEntity user = users.stream().filter(u->u.getEmail().equals(userDTO.getEmail())).findFirst().orElse(null);
-        if(user == null) return "User with this email id doesnot exist";
+        if(user == null) return ResponseEntity.badRequest().body("User with this email id doesnot exist");
         else{
 
         UserBookBusRequestDTO userBookBusRequestDTO = new UserBookBusRequestDTO();
@@ -75,13 +79,12 @@ public class UserController {
         userBookBusRequestDTO.setGender(user.getGender());
         userBookBusRequestDTO.setUserResponseDTO(userDTO.getUserResponseDTO());
 
-
-
+        return ResponseEntity.ok(bookingClient.bookBus(userBookBusRequestDTO));
 
         }
 
 
 
-        return null;
+//        return null;
     }
 }
